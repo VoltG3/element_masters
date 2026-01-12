bb28da43515d066833e8bb649067dc2964d4f010
VoltG3 <kristiandanoss@gmail.com>
2026-01-12 21:27:28 +0100

    [ UPDATE ] - immplement logo

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
+const PUBLIC_URL = (typeof process !== 'undefined' && process.env?.PUBLIC_URL) || '';
+
 const config = {
     apiUrl: (typeof process !== 'undefined' && process.env?.REACT_APP_API_URL) || 'http://localhost:8000',
 
     logo: {
-        URL_LOGO_01: '/assets/logo/Logo1.png',
-        URL_LOGO_02: '/assets/logo/Logo2.png',
-        URL_LOGO_03: '/assets/logo/Logo3.png',
-        URL_LOGO_04: '/assets/logo/Logo4.png',
-        URL_LOGO_05: '/assets/logo/Logo5.png',
+        URL_LOGO_01: `${PUBLIC_URL}/assets/logo/Logo1.png`,
+        URL_LOGO_02: `${PUBLIC_URL}/assets/logo/Logo2.png`,
+        URL_LOGO_03: `${PUBLIC_URL}/assets/logo/Logo3.png`,
+        URL_LOGO_04: `${PUBLIC_URL}/assets/logo/Logo4.png`,
+        URL_LOGO_05: `${PUBLIC_URL}/assets/logo/Logo5.png`,
     },
 
 };
